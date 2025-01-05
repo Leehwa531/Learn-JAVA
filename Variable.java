a6f9cb2c4bfaa57a033c4d3be7b28a46b3f949d5
Leehwa531 <qwg531@naver.com>
2025-01-05 15:42:25 +0900

    learn:25년1월5일

---
+++ Variable.java
@@ -0,0 +1,25 @@
+public class Variable {
+  public static void main(String[] args) {
+    //정수형 변수
+    int age1 = 27;
+
+    //실수형 변수
+    double age2 = 27.1;
+
+    //문자형 변수
+    char A = 'A';
+
+    //문자열 변수
+    String name = "Park JH";
+
+    //논리형 변수
+    boolean isSSAFY = true;
+
+    //변수 출력
+    System.out.println("Age: " + age1);
+    System.out.println("Age2: " + age2);
+    System.out.println("A: " + A);
+    System.out.println("Name: "+ name);
+    System.out.println("Is SSAFY?: " + isSSAFY);
+  }
+}
